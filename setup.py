33934e2f2f1aaa79b69b8956296e402d71306b37
Robert Walls <rjwalls@cs.umass.edu>
2014-03-25 20:05:49 -0400

    Refactored from ypr to yapr.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.version_info[0] == 2 and sys.version_info[1] < 7:
     requires += ['argparse']
 
 setup(
-    name = 'ypr',
+    name = 'yapr',
     version = '0.1',
     packages = find_packages(),
     author = __author__,
@@ -22,7 +22,7 @@ setup(
     install_requires = requires,
     entry_points = {
         'console_scripts': [
-            'yapr = ypr.yapr:main'
+            'yapr = yapr.yapr:main'
         ],
     }
 )
